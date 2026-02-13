a6bcfc0d44adac2b94060ef21583a7d22b3cc9d2
Bruno Lima <brunoluislima19@gmail.com>
2026-02-13 16:32:21 -0300

    add transparent nvim

--- configs/nvim/lua/plugins/colorscheme.lua
+++ configs/nvim/lua/plugins/colorscheme.lua
@@ -1,6 +1,9 @@
 return {
-  "craftzdog/solarized-osaka.nvim",
-  lazy = false,
-  priority = 1000,
-  opts = {},
+	"craftzdog/solarized-osaka.nvim",
+	lazy = false,
+	priority = 1000,
+	opts = {
+		transparent = true,
+	},
 }
+
