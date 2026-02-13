c0f1e513f29b4eb3f342b241b5321506fc02870c
Bruno Lima <brunoluislima19@gmail.com>
2026-02-13 13:57:43 -0300

    add package build-essential

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 # Needed for all installers
 sudo apt update -y
 sudo apt upgrade -y
-sudo apt install -y curl git
+sudo apt install -y curl git build-essential
 
 # Get the directory where this script is located
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
